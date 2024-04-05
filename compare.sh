d8cc8ebe147b2acc1b846e53a7a48a34012d34b7
adithya-a123 <149694769+adithya-a123@users.noreply.github.com>
2024-04-05 10:39:27 +0530

    Add files via upload

---
+++ compare.sh
@@ -0,0 +1,11 @@
+echo "enter first string:"
+read str1
+echo "enter second string:"
+read str2
+if [ "$str1" = "$str2" ]
+then 
+  echo "String are equal"
+else
+  echo "String are not equal"  
+fi
+  
